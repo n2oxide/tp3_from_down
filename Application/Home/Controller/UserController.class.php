a5cced322aa0b0d71a8b5d4ded53365236339fc7
n2oxide <314808362@qq.com>
2019-03-27 10:46:14 +0800

    changed show style in UserController from dump() to error and success()

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -56,17 +56,18 @@ class UserController extends Controller
             $data['name'] = I('request.name');
             //自动验证，完成
             if (!$User->create($data)) {
-                $this->ajaxReturn(array('error' => $User->getError()));
+                $this->error(array('error' => $User->getError()));
             } else {
                 //抛出PDO错误
+                //在使用ajax时，如果不捕捉错误，将会得到404页面响应
                 try {
                     $data = $User->fetchSql(false)->add();
                 } catch (Exception $exception) {
-                    $this->ajaxReturn(array('error' => $exception));
+                    $this->error(array('error' => $exception));
                 }
                 //成功并返回用户信息
                 $result['success'] = $User->find($data);
-                $this->ajaxReturn($result);
+                $this->success($result);
             }
         }
     }
@@ -116,21 +117,24 @@ class UserController extends Controller
         $User = D('Users');
         $data['id'] = I('get.id');
 
-        dump($data);
+
         if (!$User->create($data)) {
             dump($User->getError());
         }
         //以下用法会取表第一行数据，在数据读取中，必须用where显式指定条件
         //$user = $User->find();
         $user = $User->where($data)->find();
-        dump($user);
+
+        //查询出错
         if ($user === false) {
             dump($User->getDbError());
             dump($User->getLastSql());
         }
+        //待删除用户实际不存在
         if (empty($user)) {
             dump('无此用户');
         }
+        //删除用户
         if ($user !== false && !empty($user)) {
             dump($User->fetchSql(true)->delete());
         }
